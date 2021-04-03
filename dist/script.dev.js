83920d3419ddb7504558aef34ba5e18eb386c542
shorpee <mosopewiliamson@gmail.com>
2021-04-03 11:55:46 +0100

    added javascript

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,29 +1,42 @@
 "use strict";
 
-var Password = document.querySelector('password');
-var showPassword = document.querySelector('feather-eye-off');
-var hidePassword = document.querySelector('feather-eye');
-var type = Password.getAttribute('type'); //function to toggle password visibility
+var password = document.querySelector('password'); // const togglePassword = document.querySelector('toggle-word')
+// const showPassword = document.querySelector('feather-eye-off');
+// const hidePassword = document.querySelector('feather-eye');
+// togglePassword.addEventListener('click', (e) => {
+//     const type = password.getAttribute('type') === 'password'? 'text' : 'password';
+//     password.setAttribute('type', type);
+//     e.preventDefault();
+//     showPassword.toggle();
+//     hidePassword.toggle();
+// })
 
-showPassword.addEventListener('click', function (e) {
-  if (type === 'password') {
-    Password.setAttribute('text');
-    showPassword.classList.add('hide');
-    hidePassword.classList.add('show');
-  } else {
-    Password.setAttribute('password');
-    showPassword.classList.add('show');
-    hidePassword.classList.add('hide');
-  }
-});
-hidePassword.addEventListener('click', function (e) {
-  if (type === 'text') {
-    Password.setAttribute('password');
-    showPassword.classList.add('show');
-    hidePassword.classList.add('hide');
-  } else {
-    Password.setAttribute('text');
-    showPassword.classList.add('hide');
-    hidePassword.classList.add('show');
-  }
-});
+$('.toggle-password').click(function () {
+  var type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+  password.setAttribute('type', type);
+  $('#feather-eye-off').toggle();
+  $('#feather-eye').toggle();
+}); //function to toggle password visibility
+// showPassword.addEventListener('click', function(e){
+//     // const type = Password.getAttribute('type')
+//     if (type === 'password'){
+//         Password.setAttribute('text');
+//         showPassword.classList.add('hide');
+//         hidePassword.classList.add('show');
+//     } else {
+//         Password.setAttribute('password');
+//         showPassword.classList.add('show');
+//         hidePassword.classList.add('hide');
+//     }
+// });
+// hidePassword.addEventListener('click', function(e){
+//     if (type === 'text'){
+//         Password.setAttribute('password');
+//         showPassword.classList.add('show');
+//         hidePassword.classList.add('hide');
+//     } else {
+//         Password.setAttribute('text');
+//         showPassword.classList.add('hide');
+//         hidePassword.classList.add('show');
+//     }
+// });
--- script.js
+++ script.js
@@ -1,29 +1,46 @@
-const Password = document.querySelector('password');
-const showPassword = document.querySelector('feather-eye-off');
-const hidePassword = document.querySelector('feather-eye');
-const type = Password.getAttribute('type');
+const password = document.querySelector('password');
+// const togglePassword = document.querySelector('toggle-word')
+// const showPassword = document.querySelector('feather-eye-off');
+// const hidePassword = document.querySelector('feather-eye');
+
+// togglePassword.addEventListener('click', (e) => {
+//     const type = password.getAttribute('type') === 'password'? 'text' : 'password';
+//     password.setAttribute('type', type);
+//     e.preventDefault();
+//     showPassword.toggle();
+//     hidePassword.toggle();
+// })
+
+$('.toggle-password').click(function () {
+    const type = password.getAttribute('type') === 'password'? 'text' : 'password';
+    password.setAttribute('type', type);
+    $('#feather-eye-off').toggle();
+    $('#feather-eye').toggle();
+})
+
 
 //function to toggle password visibility
-showPassword.addEventListener('click', function(e){
-    if (type === 'password'){
-        Password.setAttribute('text');
-        showPassword.classList.add('hide');
-        hidePassword.classList.add('show');
-    } else {
-        Password.setAttribute('password');
-        showPassword.classList.add('show');
-        hidePassword.classList.add('hide');
-    }
-});
+// showPassword.addEventListener('click', function(e){
+//     // const type = Password.getAttribute('type')
+//     if (type === 'password'){
+//         Password.setAttribute('text');
+//         showPassword.classList.add('hide');
+//         hidePassword.classList.add('show');
+//     } else {
+//         Password.setAttribute('password');
+//         showPassword.classList.add('show');
+//         hidePassword.classList.add('hide');
+//     }
+// });
 
-hidePassword.addEventListener('click', function(e){
-    if (type === 'text'){
-        Password.setAttribute('password');
-        showPassword.classList.add('show');
-        hidePassword.classList.add('hide');
-    } else {
-        Password.setAttribute('text');
-        showPassword.classList.add('hide');
-        hidePassword.classList.add('show');
-    }
-});
+// hidePassword.addEventListener('click', function(e){
+//     if (type === 'text'){
+//         Password.setAttribute('password');
+//         showPassword.classList.add('show');
+//         hidePassword.classList.add('hide');
+//     } else {
+//         Password.setAttribute('text');
+//         showPassword.classList.add('hide');
+//         hidePassword.classList.add('show');
+//     }
+// });
