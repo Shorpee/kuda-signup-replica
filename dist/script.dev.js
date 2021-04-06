7ad90c678cf921b90c3f570ec7d3de74004fa4ac
shorpee <mosopewiliamson@gmail.com>
2021-04-06 15:36:36 +0100

    fixed password toggle button

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,24 +1,29 @@
 "use strict";
 
-var password = document.querySelector('password'); // const togglePassword = document.querySelector('toggle-word')
-// const showPassword = document.querySelector('feather-eye-off');
-// const hidePassword = document.querySelector('feather-eye');
-// togglePassword.addEventListener('click', (e) => {
-//     const type = password.getAttribute('type') === 'password'? 'text' : 'password';
-//     password.setAttribute('type', type);
-//     e.preventDefault();
-//     showPassword.toggle();
-//     hidePassword.toggle();
-// })
-
-$('.toggle-password').click(function () {
+var password = document.querySelector('#password');
+var togglePassword = document.querySelector('#toggle-password');
+var showPassword = document.querySelector('#feather-eye-off');
+var hidePassword = document.querySelector('#feather-eye');
+togglePassword.addEventListener('click', function (e) {
   var type = password.getAttribute('type') === 'password' ? 'text' : 'password';
   password.setAttribute('type', type);
-  $('#feather-eye-off').toggle();
-  $('#feather-eye').toggle();
-}); //function to toggle password visibility
-// showPassword.addEventListener('click', function(e){
-//     // const type = Password.getAttribute('type')
+  showPassword.classList.toggle('hide');
+  hidePassword.classList.toggle('hide'); // e.preventDefault();
+  // showPassword.toggle();
+  // hidePassword.toggle();
+  //     if (type === "password") {
+  //         showPassword.classList.add('hide');
+  //         hidePassword.classList.add('show');
+  //     }
+}); // $('.toggle-password').click(function () {
+//     const type = password.getAttribute('type') === 'password'? 'text' : 'password';
+//     password.setAttribute('type', type);
+//     $('#feather-eye-off').toggle();
+//     $('#feather-eye').toggle();
+// })
+// function to toggle password visibility
+// togglePassword.addEventListener('click', function(e){
+//     const type = Password.getAttribute('type')
 //     if (type === 'password'){
 //         Password.setAttribute('text');
 //         showPassword.classList.add('hide');
--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-const password = document.querySelector('password');
-// const togglePassword = document.querySelector('toggle-word')
-// const showPassword = document.querySelector('feather-eye-off');
-// const hidePassword = document.querySelector('feather-eye');
+const password = document.querySelector('#password');
+const togglePassword = document.querySelector('#toggle-password');
+const showPassword = document.querySelector('#feather-eye-off');
+const hidePassword = document.querySelector('#feather-eye');
 
-// togglePassword.addEventListener('click', (e) => {
-//     const type = password.getAttribute('type') === 'password'? 'text' : 'password';
-//     password.setAttribute('type', type);
-//     e.preventDefault();
-//     showPassword.toggle();
-//     hidePassword.toggle();
-// })
-
-$('.toggle-password').click(function () {
+togglePassword.addEventListener('click', (e) => {
     const type = password.getAttribute('type') === 'password'? 'text' : 'password';
     password.setAttribute('type', type);
-    $('#feather-eye-off').toggle();
-    $('#feather-eye').toggle();
+    showPassword.classList.toggle('hide');
+    hidePassword.classList.toggle('hide');
+    // e.preventDefault();
+    // showPassword.toggle();
+    // hidePassword.toggle();
+
+
+//     if (type === "password") {
+//         showPassword.classList.add('hide');
+//         hidePassword.classList.add('show');
+//     }
 })
 
+// $('.toggle-password').click(function () {
+//     const type = password.getAttribute('type') === 'password'? 'text' : 'password';
+//     password.setAttribute('type', type);
+//     $('#feather-eye-off').toggle();
+//     $('#feather-eye').toggle();
+// })
 
-//function to toggle password visibility
-// showPassword.addEventListener('click', function(e){
-//     // const type = Password.getAttribute('type')
+
+// function to toggle password visibility
+// togglePassword.addEventListener('click', function(e){
+//     const type = Password.getAttribute('type')
 //     if (type === 'password'){
 //         Password.setAttribute('text');
 //         showPassword.classList.add('hide');
